5c69765ba7543979b2e5019700b4eb3df1d5e30d
luis <c.luis852@gmail.com>
2026-02-13 17:52:56 +0100

    committing again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:"/photosinbarcelona",
+    base:"/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
